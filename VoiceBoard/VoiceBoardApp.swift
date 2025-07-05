818da902c40a4fea1b6b400a297ef11e1be9513c
Richcammunity <rich@cammunity.co.uk>
2025-07-05 14:21:39 +0100

    Initial Commit

---
+++ VoiceBoard/VoiceBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceBoardApp.swift
+//  VoiceBoard
+//
+//  Created by Admin on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
